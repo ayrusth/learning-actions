b2b51f8592bb0e9f9b128815ae189cb1171aae39
ayrusth <102286294+ayrusth@users.noreply.github.com>
2025-01-17 11:24:09 +0400

    changed eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,19 @@
+// import { FlatCompat } from '@eslint/eslintrc'
+// const compat = new FlatCompat({
+//   // import.meta.dirname is available after Node.js v20.11.0
+//   baseDirectory: import.meta.dirname,
+// })
+// const eslintConfig = [
+//   ...compat.config({
+//     extends: ['next'],
+//     rules: {
+//       'react/no-unescaped-entities': 'off',
+//       '@next/next/no-page-custom-font': 'off',
+//     },
+//   }),
+// ]
+// export default eslintConfig
+
 import { FlatCompat } from '@eslint/eslintrc'
 const compat = new FlatCompat({
   // import.meta.dirname is available after Node.js v20.11.0
@@ -5,11 +21,7 @@ const compat = new FlatCompat({
 })
 const eslintConfig = [
   ...compat.config({
-    extends: ['next'],
-    rules: {
-      'react/no-unescaped-entities': 'off',
-      '@next/next/no-page-custom-font': 'off',
-    },
+    extends: ['next/core-web-vitals'],
   }),
 ]
 export default eslintConfig
